41b803e4229b96d0f7c21475ab157af952f73c86
Alex <jegorov.aleksandr@yahoo.com>
2016-12-31 20:44:31 +0100

    1.4 Sluitingsdagen3

---
+++ FrituurFrida/WebContent/WEB-INF/JSP/index.jsp
@@ -0,0 +1,13 @@
+<%@page contentType='text/html' pageEncoding='UTF-8' session='false'%>
+<!doctype html>
+<html lang="nl">
+<head>
+<title>Frituur Frida</title>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<link rel="icon" href="images/favicon.ico">
+<link rel="stylesheet" href="styles/styleFrituur.css">
+</head>
+<body>
+	<h1>Vandaag zijn we ${openGesloten}</h1>
+</body>
+</html>
--- FrituurFrida/src/be/vdab/servlets/IndexServlet.java
+++ FrituurFrida/src/be/vdab/servlets/IndexServlet.java
@@ -1,9 +1,8 @@
 package be.vdab.servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.time.DayOfWeek;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,21 +10,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-//@WebServlet("/IndexServlet/index.htm")
+@WebServlet("/index.htm")
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static final String VIEW = "/WEB-INF/JSP/index.jsp";
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		PrintWriter out = response.getWriter();
-		out.println("<!doctype html>");
-		out.println("<html lang='nl'><head>");
-		out.println("<title>Frituur Frida</title></head>");
-		out.println("<body><h1>");
-		DayOfWeek dag = LocalDate.now().getDayOfWeek();
-		out.print(dag == DayOfWeek.MONDAY || dag == DayOfWeek.THURSDAY ? "Vandaag zijn we gesloten"
-				: "Vandaag zijn we open");
-		out.println("</h1></body></html>");
+		DayOfWeek dag = LocalDateTime.now().getDayOfWeek();
+		request.setAttribute("openGesloten", dag == DayOfWeek.MONDAY || dag == DayOfWeek.THURSDAY
+				? "gesloten" : "open");
+		request.getRequestDispatcher(VIEW).forward(request, response);
 	}
 
 }
