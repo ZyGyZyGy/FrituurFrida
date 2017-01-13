a5953b0c13a5495e1a2cde4f65e492a57e9f1aa6
Alex <jegorov.aleksandr@gmail.com>
2017-01-13 13:01:51 +0100

    1.11 Ingrediënten - corrected

--- FrituurFrida/WebContent/WEB-INF/JSP/sauzenmetingredient.jsp
+++ FrituurFrida/WebContent/WEB-INF/JSP/sauzenmetingredient.jsp
@@ -13,18 +13,17 @@
 	<c:import url="/WEB-INF/JSP/menu.jsp" />
 	<h1>Sauzen met ingredi&euml;nt</h1>
 	<form>
-		<label>Ingredi&euml;nt<span>${fouten.ingredient}</span> 
+		<label>Ingredi&euml;nt
 			<input name="ingredient" value="${param.ingredient}" type="text" placeholder="Typ een ingredi&euml;nt in" required autofocus>
-		</label> <input type="submit" value="Zoeken">
+		</label> <input type="submit" value="Zoeken"><br>
+		<label><span class="foutLeeg">${fouten}</span></label>
 	</form>
 	<c:if test="${not empty sauzen}">
 		<ul class="saus">
 			<c:forEach var="saus" items="${sauzen}">
 				<li><img src=" <c:url value='/images/${saus.naam}.png'/> " alt="${saus.naam}"><strong>${saus.naam}:</strong>
-					<c:forEach var="ingredient" items="${saus.ingredienten}"
-						varStatus="status"
-					>${ingredient}  
- 					<c:if test="${!status.last}">, </c:if>
+					<c:forEach var="ingredient" items="${saus.ingredienten}" varStatus="status">${ingredient}  
+ 						<c:if test="${!status.last}">, </c:if>
 					</c:forEach></li>
 			</c:forEach>
 		</ul>
--- FrituurFrida/src/be/vdab/servlets/SauzenMetIngredient.java
+++ FrituurFrida/src/be/vdab/servlets/SauzenMetIngredient.java
@@ -1,8 +1,6 @@
 package be.vdab.servlets;
 
 import java.io.IOException;
-import java.util.Collections;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,25 +17,10 @@ public class SauzenMetIngredient extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
 	    throws ServletException, IOException {
-//	if (request.getQueryString() != null) {
-//	    Map<String, String> fouten = new HashMap<>();
-//	    String ingredient = null;
-//	    try {
-//		ingredient = request.getParameter("ingredient");
-//	    } catch (Exception e) {
-//		fouten.put("ingredient", "tik een ingredient");
-//	    }
-//	    if (fouten.isEmpty()) {
-//		request.setAttribute("sauzen", sausRepository.findByIngredient(ingredient));
-//	    } else {
-//		request.setAttribute("fouten", fouten);
-//	    }
-//	}
-	
 	String ingredient = request.getParameter("ingredient");
 	if (ingredient != null) {
 	    if (ingredient.isEmpty()) {
-		request.setAttribute("fouten", Collections.singletonMap("ingredient", "verplicht"));
+		request.setAttribute("fouten", "ingredient verplicht in te vullen");
 	    } else {
 		request.setAttribute("sauzen", sausRepository.findByIngredient(ingredient));
 	    }
